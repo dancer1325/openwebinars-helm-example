c27abfc5eb8d3598a6423a739705b5d7201648d8
dancer1325 <alfredotic0809@gmail.com>
2022-12-01 13:51:33 +0100

    refactor(Section4): add commentaries about scripts

--- build.sh
+++ build.sh
@@ -5,4 +5,6 @@ docker build -t "localhost:32000/myapp:$VERSION" .
 docker push "localhost:32000/myapp:$VERSION"
 # Problems: microk8s Get "http://localhost:32000/v2/": dial tcp [::1]:32000: connect: connection refused
 helm dependency update helm-chart
+# Download the chart's dependencies and add them to your chart
 helm push helm-chart --version="$VERSION" myrepo
+# Push the chart to a private chart repo
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 VERSION=$1
 ENVIRONMENT=$2
+# numeric value to distinguish the stage
 CUSTOM_VAR=$3
 
 echo "Desplegando version $VERSION en el entorno $ENVIRONMENT"
 helm repo update
+# Guarantee that latest chart versions are selected
 helm upgrade --install "$ENVIRONMENT-app-example" myrepo/app-example \
 --namespace $ENVIRONMENT \
 --version $VERSION \
 --set image=localhost:32000/myapp:$VERSION \
 --set customVar=$CUSTOM_VAR
+# helm upgrade --install  === helm install or update in case it exists
 # image     === image pushed to the docker registry
